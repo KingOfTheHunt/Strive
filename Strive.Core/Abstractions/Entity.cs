53a8e95cc7eb8b2742b249de549481055cdf5b5e
Davi Francisco <davifrancisco214@outlook.com>
2025-08-26 08:24:06 -0300

    refactor: add Id for the Entity

--- Strive.Core/Abstractions/Entity.cs
+++ Strive.Core/Abstractions/Entity.cs
@@ -2,4 +2,7 @@ using Flunt.Notifications;
 
 namespace Strive.Core.Abstractions;
 
-public abstract class Entity : Notifiable<Notification>;
+public abstract class Entity : Notifiable<Notification>
+{
+    public int Id { get; set; }
+}
