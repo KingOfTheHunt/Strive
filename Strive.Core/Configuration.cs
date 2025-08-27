671eca154d453dfc4cca003caa3dccedbf0085f2
Davi Francisco <davifrancisco214@outlook.com>
2025-08-27 08:00:30 -0300

    feat: add the first migration

--- Strive.Core/Configuration.cs
+++ Strive.Core/Configuration.cs
@@ -3,4 +3,19 @@ namespace Strive.Core;
 public static class Configuration
 {
     public static string PrivateKey { get; set; } = string.Empty;
+    public static DatabaseConfiguration Database { get; } = new();
+    public static SmtpConfiguration Smtp { get; } = new();
+}
+
+public class DatabaseConfiguration
+{
+    public string Connection { get; set; } = string.Empty;
+}
+
+public class SmtpConfiguration
+{
+    public string Host { get; set; } = string.Empty;
+    public int Port { get; set; }
+    public string Login { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
