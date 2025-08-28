f7cc5f34cdfcb313d778e32bc7061379c28f7957
Davi Francisco <davifrancisco214@outlook.com>
2025-08-28 07:43:27 -0300

    docs: add Scalar Docs

--- Strive.Api/Program.cs
+++ Strive.Api/Program.cs
@@ -1,3 +1,4 @@
+using Scalar.AspNetCore;
 using Strive.Api.Extensions;
 using Strive.Application;
 using Strive.Infrastructure;
@@ -7,9 +8,16 @@ builder.AddConfiguration();
 builder.Services.AddDatabase();
 builder.Services.AddInfrastructure();
 builder.Services.AddApplication();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 app.MapUsersEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference("docs");
+}
 
 app.MapGet("/", () => "Hello World!");
 
