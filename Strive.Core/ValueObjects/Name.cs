02e6799a76c378bdd3625a22a72d4dc7340acda2
Davi Francisco <davifrancisco214@outlook.com>
2025-09-25 21:35:10 -0300

    feat: override Name.ToString() to return full name

--- Strive.Core/ValueObjects/Name.cs
+++ Strive.Core/ValueObjects/Name.cs
@@ -29,4 +29,9 @@ public class Name : ValueObject
         FirstName = firstName;
         LastName = lastName;
     }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}";
+    }
 }
