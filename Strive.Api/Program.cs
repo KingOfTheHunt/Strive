154f4774b1a36804b5717c4291b8a4835dfd14df
Davi Francisco <davifrancisco214@outlook.com>
2025-10-07 22:09:32 -0300

    docs: add Scalar docs

--- Strive.Api/Program.cs
+++ Strive.Api/Program.cs
@@ -1,19 +1,29 @@
+using Scalar.AspNetCore;
 using Strive.Api.Extensions;
+using Strive.Application;
+using Strive.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddConfiguration();
 builder.AddDatabase();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+app.MapUserEndpoints();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference("docs", options =>
+    {
+        options.Title = "Strive Api";
+    });
 }
 
 app.UseHttpsRedirection();
