8850155a3c48defbbc614022b9ab06e8b9768dc7
Davi Francisco <davifrancisco214@outlook.com>
2025-10-19 12:32:16 -0300

    test: add unit test for show workouts use case

---
+++ Strive.Tests/Workouts/UseCases/Show/HandlerTests.cs
@@ -0,0 +1,48 @@
+using Microsoft.Extensions.Logging;
+using Moq;
+using Strive.Application.Workouts.UseCases.Show;
+using Strive.Application.Workouts.UseCases.Show.Contracts;
+using Strive.Core.Entities;
+
+namespace Strive.Tests.Workouts.UseCases.Show;
+
+public class HandlerTests
+{
+    private readonly Mock<IRepository> _repositoryMock;
+    private readonly Mock<ILogger<Handler>> _loggerMock;
+    private readonly Handler _handler;
+
+    public HandlerTests()
+    {
+        _repositoryMock = new();
+        _loggerMock = new();
+        _handler = new(_repositoryMock.Object, _loggerMock.Object);
+    }
+
+    [Fact]
+    public async Task ShouldReturnTrueWhenUserHasWorkouts()
+    {
+        var workouts = new string[] { "Super treino de pernas", "Super treino de costas" };
+        var request = new Request(1);
+        _repositoryMock.Setup(r => r.GetAllWorkoutsAsync(request.UserId,
+            It.IsAny<CancellationToken>())).ReturnsAsync(workouts);
+
+        var result = await _handler.HandleAsync(request, CancellationToken.None);
+        
+        Assert.True(result.Success);
+        Assert.Equal(200, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task ShouldReturnFalseWhenUserHasNoWorkout()
+    {
+        var request = new Request(1);
+        _repositoryMock.Setup(r => r.GetAllWorkoutsAsync(request.UserId,
+            It.IsAny<CancellationToken>())).ReturnsAsync([]);
+
+        var result = await _handler.HandleAsync(request, CancellationToken.None);
+        
+        Assert.False(result.Success);
+        Assert.Equal(404, result.StatusCode);
+    }
+}
